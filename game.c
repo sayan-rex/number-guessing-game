52ccd7fcdc57b80de6f394d94587c693ba533058
sayan-rex <78193021+sayan-rex@users.noreply.github.com>
2021-12-30 22:15:47 +0530

    Created a game.c

---
+++ game.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main(){
+    int number, guess, nguesses = 1;
+    srand(time(0));
+    number = rand()%100 +1; //generate random number between 1 to 100
+    printf("The random number is %d \n", number);
+    // keep running the loop until the number is guessed
+    do{
+        printf("guess the number between 1 to 100 \n");
+        scanf("%d", &guess);
+        if(guess>number){
+            printf("Lower number please \n");
+        }
+        else if(guess<number){
+            printf("Higher number please \n");
+        }
+        else{
+            printf("you guessed in %d attempts \n", nguesses);
+        }
+        nguesses++;
+    }while(guess!=number);
+    return 0;
+}
