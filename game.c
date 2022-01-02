378eed45b00a236d2494c4f02d54e26d64d68609
sayan-rex <78193021+sayan-rex@users.noreply.github.com>
2022-01-02 15:21:48 +0530

    Update game.c

--- game.c
+++ game.c
@@ -6,7 +6,7 @@ int main(){
     int number, guess, nguesses = 1;
     srand(time(0));
     number = rand()%100 +1; //generate random number between 1 to 100
-    printf("The random number is %d \n", number);
+    //printf("The random number is %d \n", number);
     // keep running the loop until the number is guessed
     do{
         printf("guess the number between 1 to 100 \n");
